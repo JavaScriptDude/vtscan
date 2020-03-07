5df419bad42d1c9b73eae4ba645481c45b74585e
JavaScriptDude <tim.c.quinn@gmail.com>
2020-03-07 12:08:25 -0500

    Major cleanup of code and todo list

--- vtscan.py
+++ vtscan.py
@@ -16,104 +16,130 @@ from __future__ import print_function
 # Home: https://github.com/JavaScriptDude/vtscan
 # Licence: https://opensource.org/licenses/MIT
 # .: Todo :.
-# [.] Add verbosity argument --verbose | -v
-# [.] Streamline the output code
-# [.] Generate Google search URLS for the user
+# (none)
 #########################################
 
-import os, sys, json, hashlib, traceback, pathlib
+import os, sys, json, hashlib, traceback, pathlib, argparse
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 
 
-def main(argv):
+def main():
+
+    argp = argparse.ArgumentParser(prog="vtscan")
+    argp.add_argument("--verbose", "-v", action='store_true')
+    argp.add_argument("--links", "-L", action='store_true')
+    argp.add_argument("file", type=str, help="File to scan")
+
+    args = argp.parse_args()
+
+    dump_response : bool = False
+    got_results : bool = False
+    result_issues : int = -1
+    warnings : list = []
 
     # Check for Api key
     if "VT_API_KEY" not in os.environ:
-        exit("Missing Virus total API Key. Please set VT_API_KEY environment variable!", 1)
+        argp.print_help()
+        exit("\nMissing Virus total API Key. Please set VT_API_KEY environment variable!", 1)
 
     API_KEY=os.environ["VT_API_KEY"]
     if API_KEY.strip() == "":
-        exit("Missing Virus total API Key. Please set VT_API_KEY environment variable!", 1)
-
-    # Get fullpath (first arg)
-    fullpath = argv[0]
-    fname, fpath = splitPath(fullpath)
+        argp.print_help()
+        exit("\nMissing Virus total API Key. Please set VT_API_KEY environment variable!", 1)
 
     # Verify that file exists
-    if not os.path.isfile(fullpath):
-        print("Please specify path to an existing file")
+    if not os.path.isfile(args.file):
+        argp.print_help()
+        exit("\nPlease specify path to an existing file", 1)
+
+    # Get args.file (first arg)
+    fname, fpath = splitPath(args.file)
 
     # Get sha1 checksum of file
-    digest_md5 = getChecksum(fullpath, 'md5')
-    digest_sha1 = getChecksum(fullpath, 'sha1')
-    digest_sha256 = getChecksum(fullpath, 'sha256')
+    digest_md5 = getChecksum(args.file, 'md5')
+    digest_sha1 = getChecksum(args.file, 'sha1')
+    digest_sha256 = getChecksum(args.file, 'sha256')
 
     vt = VirusTotalPublicApi(API_KEY)
 
     response = vt.get_file_report(digest_sha256)
 
     if not response['response_code'] == 200:
-        exit("Bad general response_code from virus total: {}".format(json.dumps(response, sort_keys=False, indent=4)), 1)
-
-    res = response['results']
-    if not res['response_code'] == 1:
-        if res['resource'] == '87b566abab9888ff362058f90818f8dae6cf3e2a67de645e446a2999983a91a2':
-            exit("""
-.: VTScan Summary :.
-- File: {0}
-- Path: {1}
-- md5: {2}
-- sha1: {3}
-- sha256: {4}
-- Warning: {5}
--          {6}""".format(
-                    fname, fpath
-                    ,digest_md5, digest_sha1, digest_sha256
-                    ,"File not found in VirusTotal database. Therefore its safety is unknown."
-                    ,"Check if the download source has checksums to verify or Try googling one or all the checksums."
-            ), 1)
-        else:
-            exit("Bad result response_code from virus total: {}".format(json.dumps(res, sort_keys=False, indent=4)), 1)
+        dump_response = True
+        warnings.append("Bad general response_code from Virus Total")
 
 
-    # print("Raw virus total results: {}".format(json.dumps(res, sort_keys=False, indent=4)), 1)
-    
+    if len(warnings) == 0: # Dig into the results...
+        res = response['results']
+        if not res['response_code'] == 1:
+            dump_response = True
+            if res['resource'] == '87b566abab9888ff362058f90818f8dae6cf3e2a67de645e446a2999983a91a2':
+                warnings.append("File not found in VirusTotal database. Therefore its safety is unknown.")
+                warnings.append("Check if the download source has checksums to verify or Try googling one or all the checksums.")
+            else:
+                warnings.append("Bad result response_code from virus total: {}")
 
-    # Lets be paranoid and verify the checksums found
-    warnings: list = []
-    if not res['md5'] == digest_md5:
-        warnings.append("MD5 Checksums do not match:\noriginal: {}\nreturned: {}".format(digest_md5, res['md5']))
-    if not res['sha1'] == digest_sha1:
-        warnings.append("SHA1 Checksums do not match:\noriginal: {}\nreturned: {}".format(digest_sha1, res['sha1']))
-    if not res['sha256'] == digest_sha256:
-        warnings.append("SHA256 Checksums do not match:\noriginal: {}\nreturned: {}".format(digest_sha256, res['sha256']))
+        # print("Raw virus total results: {}".format(json.dumps(res, sort_keys=False, indent=4)), 1)
         
+        if len(warnings) == 0:
+            # Lets be paranoid and verify the checksums found
+            if not res['md5'] == digest_md5:
+                warnings.append("MD5 Checksums do not match:\n -    Original: {}\n - Virus Total: {}".format(digest_md5, res['md5']))
+            if not res['sha1'] == digest_sha1:
+                warnings.append("SHA1 Checksums do not match:\n -    Original: {}\n - Virus Total: {}".format(digest_sha1, res['sha1']))
+            if not res['sha256'] == digest_sha256:
+                warnings.append("SHA256 Checksums do not match:\n -    Original: {}\n - Virus Total: {}".format(digest_sha256, res['sha256']))
+            
+            got_results = True
+            result_issues = res['positives']
 
-    issues = res['positives']
+
+    if dump_response or args.verbose:
+        print(".: Raw Virus Total Response :.\n" + json.dumps(response, sort_keys=False, indent=4) + "\n~\n")
 
     print("""
-.: VTScan Summary :.
--   File: {0}
--   Path: {1}
--    md5: {2}
--   sha1: {3}
-- sha256: {4}
-- issues: {5} out of {6} {7}
-- {8}
-~      """.format(
-             fname, fpath
-            ,digest_md5, digest_sha1, digest_sha256
-            ,issues, res['total'], ('(see above)' if issues > 0 else '(100% pass)' )
-            ,"{} {}".format( ('permalink:' if issues == 0 else 'Navigate to for more info:'), res['permalink'])
-    ))
+.: Details :.
+- md5: {0}
+- sha1: {1}
+- sha256: {2}""".format(digest_md5, digest_sha1, digest_sha256) )
+
+    if got_results:
+        print("- Permalink: " + res['permalink'])
+
+    if args.links or not got_results or result_issues > 0:
+        print("""
+.: Search Links for hand testing :.
+- Google MD5: https://www.google.com/search?q=%22{0}%22
+- Google SHA1: https://www.google.com/search?q=%22{1}%22
+- Bing MD5: https://www.bing.com/search?q=%22{0}%22
+- Bing SHA1: https://www.bing.com/search?q=%22{1}%22""".format(digest_md5,digest_sha1))
+
+    print("""
+.: File :.
+- File: {0}
+- Path: {1}""".format(fname, fpath) )
+
+
+    if got_results:
+        print("\n.: Virus Total Summary :.")
+        if result_issues == 0:
+            print("- Detections: 0 out of {} (100% pass)".format(res['total']))
+        else:
+            print("- Detections: {} out of {} (Go to VirusTotal for more details)".format(result_issues, res['total']))
+
+    
+
 
     if len(warnings) > 0:
-        print("Possible API Issues Detected in Python:\n{}\n~".format('\n'.join(warnings)))
+        print(".: Warnings :.")
+        for i, warning in enumerate(warnings):
+            print(" {}) {}".format(i, warning))
+
+    print("~")
 
 
 
-
-def getChecksum(fullpath, csumtype):
+def getChecksum(path, csumtype):
     if csumtype == 'md5':
         h  = hashlib.md5()
     elif csumtype == 'sha1':
@@ -124,7 +150,7 @@ def getChecksum(fullpath, csumtype):
         raise Exception("Unexpected csumtype: {}".format(csumtype))
     b  = bytearray(128*1024)
     mv = memoryview(b)
-    with open(fullpath, 'rb', buffering=0) as f:
+    with open(path, 'rb', buffering=0) as f:
         for n in iter(lambda : f.readinto(mv), 0):
             h.update(mv[:n])
     return h.hexdigest()
@@ -153,6 +179,7 @@ def toPosixPath(s:str, strip_slash:bool=False, ensure_slash:bool=False):
         return '%s/' % s
     return s
 
+
 def getAbsPath(s:str):
     return os.path.abspath( pathlib.Path(s).expanduser() )
 
@@ -160,9 +187,9 @@ def getAbsPath(s:str):
 if __name__ == '__main__':
     iExit = 0
     try:
-        main(sys.argv[1:])
+        main()
     except Exception:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         aTB = traceback.format_tb(exc_traceback)
-        exit("Fatal exception: {}\n - msg: {}\n stack: {}".format(exc_type, exc_value, '\n'.join(aTB)), exitCode=1)
+        exit("Program Exception:\nStack:\n{}\n Error: {} - {}".format('\n'.join(aTB), exc_type.__name__, exc_value), exitCode=1)
     sys.exit(iExit)
